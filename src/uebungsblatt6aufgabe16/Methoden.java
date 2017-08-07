934a6150151f2661fff5eb7e73052e3db4171924
s.kuebel <s.kuebel@otterbach.de>
2017-08-07 15:05:42 +0200

    Erste Vorgaben eingefüllt

---
+++ src/uebungsblatt6aufgabe16/Methoden.java
@@ -0,0 +1,22 @@
+package uebungsblatt6aufgabe16;
+
+import java.util.Collection;
+
+public class Methoden {
+	  public static double doubleValue(Object o) {
+	    return ((Double) o).doubleValue();
+	  }
+	  public static double mittelwert (Collection c) {
+		
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  return 0;	  
+		  
+	} 
+
+}
---
+++ src/uebungsblatt6aufgabe16/Mittelwerthaupt.java
@@ -0,0 +1,13 @@
+package uebungsblatt6aufgabe16;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class Mittelwerthaupt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
